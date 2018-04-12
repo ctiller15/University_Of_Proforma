f17d7a84ab4b806e6896a169a43e5a7116155f8f
Tiller <ctiller@proforma.com>
2018-04-12 14:47:38 -0400

    first commit

---
+++ Admin/Admin/Professor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    class Professor
+    {
+        public int Professor_ID { get; set; }
+        public string Professor_Name { get; set; }
+        public string Professor_Title { get; set; }
+        // Contains professor ID
+        // Contains professor Name
+        // Contains professor Title
+
+        public Professor()
+        {
+
+        }
+    }
+}
---
+++ Admin/Admin/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    class Program
+    {
+        static void InsertProfessor(SqlConnection conn, Professor prof)
+        {
+            var _insert = "INSERT INTO PROFESSORS (Name, Title)" +
+                "VALUES (@Name, @Title)";
+
+            var cmd = new SqlCommand(_insert, conn);
+
+            cmd.Parameters.AddWithValue("Name", prof.Professor_Name);
+            cmd.Parameters.AddWithValue("Title", prof.Professor_Title);
+            cmd.ExecuteScalar();
+        }
+
+        static void Main(string[] args)
+        {
+            // Get the database location.
+            const string CONNECTION_STRING =
+    @"Server=localhost\SQLEXPRESS;Database=University_Of_Proforma;Trusted_Connection=True;";
+
+            // Open the connection
+            using (var conn = new SqlConnection(CONNECTION_STRING))
+            {
+                var newProf = new Professor
+                {
+                    Professor_Name = "Ordetx",
+                    Professor_Title = "Mrs."
+                };
+
+                var newerProf = new Professor
+                {
+                    Professor_Name = "Costas",
+                    Professor_Title = "Dr.",
+                };
+                conn.Open();
+                InsertProfessor(conn, newProf);
+                InsertProfessor(conn, newerProf);
+
+            }
+        }
+    }
+}
