5d32f140a0b82abb3d98ca6db084ed1fb7a0c4f8
Tiller <ctiller@proforma.com>
2018-04-13 10:04:43 -0400

    Add basic validation function to check year

--- Admin/Admin/DateTimeOperations.cs
+++ Admin/Admin/DateTimeOperations.cs
@@ -16,10 +16,16 @@ namespace Admin
             string Start_Hour;
             string Start_Minute;
 
+            bool isValidDate = false;
             // Getting date informtion
 
-            Console.WriteLine("What is the Start Year?");
-            Start_Year = Console.ReadLine();
+            while (!isValidDate)
+            {
+                Console.WriteLine("What is the Start Year?");
+                Start_Year = Console.ReadLine();
+                isValidDate = CheckValidBounds(Start_Year, 0, int.MaxValue);
+            }
+
 
             Console.WriteLine("What is the Start Month?\n");
             Start_Month = Console.ReadLine();
@@ -32,6 +38,30 @@ namespace Admin
 
             Console.WriteLine("What is the Start Minute? (0-59)");
             Start_Minute = Console.ReadLine();
+        }
+
+        static public bool CheckValidBounds(string value, int lowerBound, int upperBound)
+        {
+            // Check if string can be a valid integer...
+            if(Int32.TryParse(value, out int val)){
+                // If so, compare it to the bounds.
+                int checkedVal = Int32.Parse(value);
+                if(lowerBound < checkedVal && upperBound > checkedVal)
+                {
+                    Console.WriteLine("This is a valid number!");
+                    // Return True;
+                    return true;
+                } else
+                {
+                    Console.WriteLine("This is not a valid number. Please try again.");
+                    // Return False;
+                    return false;
+                }
+            } else
+            {
+                Console.WriteLine("You did not submit a valid number. Please Try again.");
+                return false;
+            }
         }
     }
 }
