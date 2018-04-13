0a2d7ed7ca4883cbc34b0136dc9bc03103198955
Tiller <ctiller@proforma.com>
2018-04-13 10:15:22 -0400

    Run date validation on every piece of the user-submitted date

--- Admin/Admin/DateTimeOperations.cs
+++ Admin/Admin/DateTimeOperations.cs
@@ -21,23 +21,43 @@ namespace Admin
 
             while (!isValidDate)
             {
-                Console.WriteLine("What is the Start Year?");
+                Console.WriteLine("What is the Start Year?\n");
                 Start_Year = Console.ReadLine();
                 isValidDate = CheckValidBounds(Start_Year, 0, int.MaxValue);
             }
+            isValidDate = false;
 
+            while(!isValidDate)
+            {
+                Console.WriteLine("What is the Start Month? (1-12)\n");
+                Start_Month = Console.ReadLine();
+                isValidDate = CheckValidBounds(Start_Month, 1, 12);
+            }
+            isValidDate = false;
 
-            Console.WriteLine("What is the Start Month?\n");
-            Start_Month = Console.ReadLine();
+            while (!isValidDate)
+            {
+                Console.WriteLine("What is the Start Day?\n");
+                Start_Day = Console.ReadLine();
+                isValidDate = CheckValidBounds(Start_Day, 1, 31);
+            }
+            isValidDate = false;
 
-            Console.WriteLine("What is the Start Day?\n");
-            Start_Day = Console.ReadLine();
+            while (!isValidDate)
+            {
+                Console.WriteLine("What is the Start Hour? (0-23)\n");
+                Start_Hour = Console.ReadLine();
+                isValidDate = CheckValidBounds(Start_Hour, 0, 23);
+            }
+            isValidDate = false;
 
-            Console.WriteLine("What is the Start Hour? (0-24)");
-            Start_Hour = Console.ReadLine();
+            while (!isValidDate)
+            {
+                Console.WriteLine("What is the Start Minute? (0-59)\n");
+                Start_Minute = Console.ReadLine();
+                isValidDate = CheckValidBounds(Start_Minute, 0, 59);
+            }
 
-            Console.WriteLine("What is the Start Minute? (0-59)");
-            Start_Minute = Console.ReadLine();
         }
 
         static public bool CheckValidBounds(string value, int lowerBound, int upperBound)
@@ -46,7 +66,7 @@ namespace Admin
             if(Int32.TryParse(value, out int val)){
                 // If so, compare it to the bounds.
                 int checkedVal = Int32.Parse(value);
-                if(lowerBound < checkedVal && upperBound > checkedVal)
+                if(lowerBound <= checkedVal && upperBound >= checkedVal)
                 {
                     Console.WriteLine("This is a valid number!");
                     // Return True;
