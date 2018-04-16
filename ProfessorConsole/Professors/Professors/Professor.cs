999380162d749767a34e0e2c34c3146875547721
Tiller <ctiller@proforma.com>
2018-04-16 10:04:13 -0400

    add professors console app and add Professor, DBConnection, and SQLOperations classes to it.

---
+++ ProfessorConsole/Professors/Professors/Professor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    class Professor : ISQLOperations
+    {
+        public int Professor_ID { get; set; }
+        public string Professor_Name { get; set; }
+        public string Professor_Title { get; set; }
+
+        // Inserts SQL data into database.
+        public void Insert(SqlConnection conn)
+        {
+            var _insert = "INSERT INTO PROFESSORS (Name, Title)" +
+                "VALUES (@Name, @Title)";
+
+            var cmd = new SqlCommand(_insert, conn);
+
+            cmd.Parameters.AddWithValue("Name", this.Professor_Name);
+            cmd.Parameters.AddWithValue("Title", this.Professor_Title);
+            cmd.ExecuteScalar();
+        }
+
+        public Professor()
+        {
+
+        }
+
+        public Professor(string name, string title)
+        {
+            this.Professor_Name = name;
+            this.Professor_Title = title;
+        }
+
+        public Professor(SqlDataReader reader)
+        {
+            this.Professor_Name = reader["Name"].ToString();
+            this.Professor_Title = reader["Title"].ToString();
+        }
+    }
+}
